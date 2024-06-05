e6fe1f550c25d20e6a203a5d9db26224fe224ef6
Disarttk <mmmm@google.com>
2024-06-05 17:39:56 +0300

    Верстка формы авторизации, view-функция авторизации пользователя

--- users/views.py
+++ users/views.py
@@ -1,6 +1,27 @@
+from django.contrib import auth
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .forms import UserLoginForm
 
 
 def login(request):
-    return HttpResponse('ВХод')
+
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('main:index')
+
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, template_name='users/login.html', context=context)
